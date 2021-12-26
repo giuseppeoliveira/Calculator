81dc6c51f83fe93839f065b5257d97f23605fdba
Giuseppe Santos <giuseppe_santos@hotmail.com>
2021-12-26 18:52:42 -0300

    Adicionado números na tela

--- Calculator.js
+++ Calculator.js
@@ -1,4 +1,5 @@
-function insert(){
-    
-
+function insert(number)
+{
+    var num = document.getElementById('result').innerHTML;
+    document.getElementById('result').innerHTML = num + number;
 }
